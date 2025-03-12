ebab404a8a7fa0670d201d9919384b1789e59d8b
h70076333 <h70076222@88.com>
2025-03-12 16:59:21 +0800

    Update vnt.sh

--- trunk/user/vntcli/vnt.sh
+++ trunk/user/vntcli/vnt.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-/usr/bin/vpn --stop
+/usr/bin/vnt-cli --stop
 #关闭vnt的防火墙
 iptables -D INPUT -i hxsdwan -j ACCEPT 2>/dev/null
 iptables -D FORWARD -i hxsdwan -o hxsdwan -j ACCEPT 2>/dev/null
 iptables -D FORWARD -i hxsdwan -j ACCEPT 2>/dev/null
 iptables -t nat -D POSTROUTING -o hxsdwan -j MASQUERADE 2>/dev/null
-killall vpn
-killall -9 vpn
+killall vnt-cli
+killall -9 vnt-cli
 sleep 4
 #清除vnt的虚拟网卡
 ifconfig hxsdwan down && ip tuntap del hxsdwan mode tun
@@ -23,14 +23,13 @@ vntcli_localadd=$(nvram get vntcli_localadd)
 echo $vntcli_localadd
 vntcli_serverw=$(nvram get vntcli_serverw)
 echo $vntcli_serverw
-
 lan_ipaddr=$(nvram get lan_ipaddr) 
 echo $lan_ipaddr
 
-/usr/bin/vpn -k $vntcli_token $vntcli_serverw -d $vntcli_desname --nic hxsdwan -i $vntcli_localadd -o $lan_ipaddr/24 --ip $vntcli_ip &
+/usr/bin/vnt-cli -k $vntcli_token $vntcli_serverw -d $vntcli_desname --nic hxsdwan -i $vntcli_localadd -o $lan_ipaddr/24 --ip $vntcli_ip &
 
 sleep 4
-if [ ! -z "`pidof vpn`" ] ; then
+if [ ! -z "`pidof vnt-cli`" ] ; then
 logger -t "异地组网" "启动成功"
 #放行vpn防火墙
 iptables -I INPUT -i hxsdwan -j ACCEPT
