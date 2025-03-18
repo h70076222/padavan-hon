080ef22d5ca66ab63d1bf8e586399e9be6920cb8
h70076222 <142376079+h70076222@users.noreply.github.com>
2025-03-18 23:40:11 +0800

    Update dev_init.sh

--- trunk/user/scripts/dev_init.sh
+++ trunk/user/scripts/dev_init.sh
@@ -4,12 +4,12 @@ mount -t proc proc /proc
 mount -t sysfs sysfs /sys
 [ -d /proc/bus/usb ] && mount -t usbfs usbfs /proc/bus/usb
 
-size_tmp="34M"
+size_tmp="28M"
 size_var="4M"
 size_etc="6M"
 
 if [ "$1" == "-l" ] ; then
-	size_tmp="16M"
+	size_tmp="8M"
 	size_var="1M"
 fi
 
