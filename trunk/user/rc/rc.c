7345113ff3ef19afb1d235ce57cf1b553f90d991
h70076222 <142376079+h70076222@users.noreply.github.com>
2025-07-14 00:29:19 +0800

    Update rc.c

--- trunk/user/rc/rc.c
+++ trunk/user/rc/rc.c
@@ -1396,6 +1396,12 @@ handle_notifications(void)
 			restart_hxcli();
 		}
 #endif
+#if defined(APP_NELINK)
+		else if (strcmp(entry->d_name, RCN_RESTART_NELINK) == 0)
+		{
+			restart_nelink();
+		}
+#endif
 /*#if defined(APP_NPC)
 		else if (strcmp(entry->d_name, RCN_RESTART_NPC) == 0)
 		{
