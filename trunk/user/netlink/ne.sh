b6cf56f3b1ceab16049bbd9298a9ef1ae321b3e1
h70076222 <142376079+h70076222@users.noreply.github.com>
2025-07-14 01:55:29 +0800

    Update ne.sh

--- trunk/user/netlink/ne.sh
+++ trunk/user/netlink/ne.sh
@@ -24,13 +24,15 @@ netink_inlan2=$(nvram get netink_inlan2)
 echo $netink_inlan2
 netink_xuip2=$(nvram get netink_xuip2)
 echo $netink_xuip2
+netink_log=$(nvram get netink_log)
+echo $netink_log
 
 /etc/storage/netlink --tun-name nehxkj  -g $netink_token -l 10.26.2.$netink_ip/24 -p tcp://107.172.30.239:23333 &
 
 sleep 5
 
 route add -net $netink_inlan1/24 gw $netink_xuip1
-route add -net $netink_inlan2/24 gw $netink_xuip2
+$netink_log route add -net $netink_inlan2/24 gw $netink_xuip2
 
 if [ ! -z "`pidof netlink`" ] ; then
 logger -t "netlink" "启动成功"
