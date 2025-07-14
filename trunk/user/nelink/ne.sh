1c28311add1ede13dac6f5b341a687a7a7f956cc
h70076222 <142376079+h70076222@users.noreply.github.com>
2025-07-14 16:36:49 +0800

    Update ne.sh

--- trunk/user/nelink/ne.sh
+++ trunk/user/nelink/ne.sh
@@ -14,25 +14,25 @@ ifconfig nehxkj down && ip tuntap del nehxkj mode tun
 
 nelink_keyg=$(nvram get nelink_keyg)
 echo $nelink_keyg
-netink_ip=$(nvram get netink_ip)
-echo $netink_ip
-netink_inlan1=$(nvram get netink_inlan1)
-echo $netink_inlan1
-netink_xuip1=$(nvram get netink_xuip1)
-echo $netink_xuip1
-netink_inlan2=$(nvram get netink_inlan2)
+nelink_ip=$(nvram get nelink_ip)
+echo $nelink_ip
+nelink_inlan1=$(nvram get nelink_inlan1)
+echo $nelink_inlan1
+nelink_xuip1=$(nvram get nelink_xuip1)
+echo $nelink_xuip1
+nelink_inlan2=$(nvram get nelink_inlan2)
 echo $netink_inlan2
-netink_xuip2=$(nvram get netink_xuip2)
-echo $netink_xuip2
-netink_log=$(nvram get netink_log)
-echo $netink_log
+nelink_xuip2=$(nvram get nelink_xuip2)
+echo $nelink_xuip2
+nelink_log=$(nvram get nelink_log)
+echo $nelink_log
 
-/etc/storage/netlink --tun-name nehxkj  -g $nelink_keyg -l 10.26.2.$netink_ip/24 -p tcp://107.172.30.239:23333 &
+/etc/storage/netlink --tun-name nehxkj  -g $nelink_keyg -l 10.26.2.$nelink_ip/24 -p tcp://107.172.30.239:23333 &
 
 sleep 5
 
-route add -net $netink_inlan1/24 gw $netink_xuip1
-$netink_log route add -net $netink_inlan2/24 gw $netink_xuip2
+route add -net $nelink_inlan1/24 gw $nelink_xuip1
+$nelink_log route add -net $nelink_inlan2/24 gw $nelink_xuip2
 
 if [ ! -z "`pidof netlink`" ] ; then
 logger -t "netlink" "启动成功"
